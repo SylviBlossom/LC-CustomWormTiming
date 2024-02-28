91d136f49c6649413c40826cf74a13c855353b55
SylviBlossom <sylviblossom@gmail.com>
2024-02-28 13:52:22 -0700

    Release v1.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+### v1.0.2
+- Update to CSync v2.0.0
+
 ### v1.0.1
 - Fix mistake in descriptions (Vanilla max is 2 seconds, not 2.33 seconds)
 
--- src/PluginInfo.cs
+++ src/PluginInfo.cs
@@ -4,5 +4,5 @@ public static class PluginInfo
 {
     public const string PLUGIN_GUID = "SylviBlossom.CustomWormTiming";
     public const string PLUGIN_NAME = "CustomWormTiming";
-    public const string PLUGIN_VERSION = "1.0.1";
+    public const string PLUGIN_VERSION = "1.0.2";
 }
